57a9b0d900abc3c0cc216c1500a94090bdec2992
Sylvance <kerandisylvance@gmail.com>
2018-01-10 22:34:35 +0300

    Add apiary script
    [Finishes #154223465]

---
+++ app/static/js/apiary.js
@@ -0,0 +1,13 @@
+new Apiary.Embed({
+    "subdomain": "sylvance1",
+    "preferences": {
+        "element": "#documentation",
+        "displayHttpMethods": true
+    },
+    "theme": {
+        "fontFamily": "Roboto"
+    },
+    "customFonts": {
+        "googleFonts": "https://fonts.googleapis.com/css?family=Roboto"
+    }
+})
